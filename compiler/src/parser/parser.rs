14cd382ae4c491f966f5a62723cb26dcfbd425de
Matthew Dowdell <mdowdell244@gmail.com>
2019-01-18 22:11:17 +0000

    add placeholder for parser

---
+++ compiler/src/parser/parser.rs
@@ -0,0 +1,28 @@
+//!
+//!
+//!
+
+
+///
+///
+///
+struct Parser {}
+
+
+impl Parser {
+	///
+	///
+	///
+	pub fn new() -> Parser {
+		Parser {
+			// TODO
+		}
+	}
+
+	///
+	///
+	///
+	pub fn parse(&mut self) {
+		// TODO
+	}
+}
